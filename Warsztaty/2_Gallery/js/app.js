144577c473dc1c69866e74f60ca794c391b2eb68
Katherina611 <katarzynag0609@gmail.com>
2017-12-15 15:05:58 +0100

    gallery

--- Warsztaty/2_Gallery/js/app.js
+++ Warsztaty/2_Gallery/js/app.js
@@ -9,5 +9,31 @@
  </div>
  */
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('Działa');
+    var allLi=document.querySelectorAll('li');
+    var body=document.querySelector('body');
+    console.log(allLi);
+    console.log(body);
+
+
+    allLi.forEach(function(element){
+        element.addEventListener('click', function (event){
+            var div=document.createElement('div');
+            body.appendChild(div);
+            div.classList='fullScreen';
+            var img=document.createElement('img');
+            div.appendChild(img);
+            var button=document.createElement('button');
+            div.appendChild(button);
+            button.classList='close';
+            button.innerText='Close';
+            img.setAttribute('src',this.firstElementChild.getAttribute("src"));
+            button.addEventListener('click', function(event){
+                div.parentElement.removeChild(div);
+            })
+
+        })
+    });
+//Na stronie mozna byłoby dodać slider, żeby nie musieć cały czas zamykać okna, by wyświetlić kolejny obraz//
 
 });
